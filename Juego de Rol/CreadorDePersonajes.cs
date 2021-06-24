ee90f74c49f278aa08749c687e05ee3d23ac7711
lucianaveravg <luuveravg@gmail.com>
2021-06-24 00:55:28 -0300

    first commit

---
+++ Juego de Rol/CreadorDePersonajes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juego_de_Rol
+{
+    class CreadorDePersonajes
+    {
+    }
+}
---
+++ Juego de Rol/personajes.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Juego_de_Rol
+{
+    enum TipoPersonaje
+    {
+        Elfo,
+        Tauren,
+        Draenei,
+        Humano,
+        Otro
+    }
+   
+    public class Personaje
+    {
+        private TipoPersonaje tipo;
+        private string nombre;
+        private string apodo;
+        private DateTime fechaNacimiento;
+        private int edad;
+        private int salud;
+
+        private int velocidad;
+        private int destreza;
+        private int fuerza;
+        private int nivel;
+        private int armadura;
+
+        public Personaje(string nombre, string apodo, TipoPersonaje tipo, DateTime fechaNacimiento, int edad)
+        {
+
+        }
+
+        private void CrearValoresAleatorios()
+        {
+            Random random = new Random();
+            velocidad = random.Next(1, 10);
+            destreza = random.Next(1, 10);
+            armadura = random.Next(1, 10);
+            fuerza = random.Next(1, 10);
+            Nivel = 1;
+        }
+
+        public TipoPersonaje Tipo { get => tipo; set => tipo = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
+
+    }
+}
+
