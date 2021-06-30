cbdca1d3aa229398f4cd17c140c0ef14016cf7be
lucianaveravg <luuveravg@gmail.com>
2021-06-29 21:18:31 -0300

    de nuevo

---
+++ Juego de Rol.2/JuegodeRol/VentanaCrearPersonaje.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace JuegodeRol
+{
+    partial class VentanaCrearPersonaje
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nombreBox = new System.Windows.Forms.TextBox();
+            this.apodoBox = new System.Windows.Forms.TextBox();
+            this.fechaNacPick = new System.Windows.Forms.DateTimePicker();
+            this.tipoBox = new System.Windows.Forms.ComboBox();
+            this.btnCrear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 80);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Engravers MT", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(88, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Crea tu personaje";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Apodo:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 143);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Tipo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(29, 173);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(122, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Fecha de Nacimiento:";
+            // 
+            // nombreBox
+            // 
+            this.nombreBox.Location = new System.Drawing.Point(89, 77);
+            this.nombreBox.Name = "nombreBox";
+            this.nombreBox.Size = new System.Drawing.Size(100, 23);
+            this.nombreBox.TabIndex = 9;
+            // 
+            // apodoBox
+            // 
+            this.apodoBox.Location = new System.Drawing.Point(89, 112);
+            this.apodoBox.Name = "apodoBox";
+            this.apodoBox.Size = new System.Drawing.Size(100, 23);
+            this.apodoBox.TabIndex = 6;
+            // 
+            // fechaNacPick
+            // 
+            this.fechaNacPick.Location = new System.Drawing.Point(157, 173);
+            this.fechaNacPick.Name = "fechaNacPick";
+            this.fechaNacPick.Size = new System.Drawing.Size(200, 23);
+            this.fechaNacPick.TabIndex = 7;
+            // 
+            // tipoBox
+            // 
+            this.tipoBox.FormattingEnabled = true;
+            this.tipoBox.Items.AddRange(new object[] {
+            "Elfo",
+            "Tauren",
+            "Draenei",
+            "Humano"});
+            this.tipoBox.Location = new System.Drawing.Point(89, 141);
+            this.tipoBox.Name = "tipoBox";
+            this.tipoBox.Size = new System.Drawing.Size(121, 23);
+            this.tipoBox.TabIndex = 8;
+            // 
+            // btnCrear
+            // 
+            this.btnCrear.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnCrear.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCrear.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnCrear.Location = new System.Drawing.Point(192, 223);
+            this.btnCrear.Name = "btnCrear";
+            this.btnCrear.Size = new System.Drawing.Size(75, 23);
+            this.btnCrear.TabIndex = 10;
+            this.btnCrear.Text = "CREAR";
+            this.btnCrear.UseVisualStyleBackColor = false;
+            this.btnCrear.Click += new System.EventHandler(this.btnCrear_Click);
+            // 
+            // VentanaCrearPersonaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(488, 304);
+            this.Controls.Add(this.btnCrear);
+            this.Controls.Add(this.tipoBox);
+            this.Controls.Add(this.fechaNacPick);
+            this.Controls.Add(this.apodoBox);
+            this.Controls.Add(this.nombreBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "VentanaCrearPersonaje";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox nombreBox;
+        private System.Windows.Forms.TextBox apodoBox;
+        private System.Windows.Forms.DateTimePicker fechaNacPick;
+        private System.Windows.Forms.ComboBox tipoBox;
+        private System.Windows.Forms.Button btnCrear;
+    }
+}
+
